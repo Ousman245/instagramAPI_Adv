dc72b9b0019e0820b0bc036496139d80f9e4adb5
ousman245 <ocamara@cristoreyny.org>
2016-02-04 13:49:47 +0000

    Flickr Project finished click pic to see it fully

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,31 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "3565fe59fc166cb7f5a0ab9b7c982cc8";
 
 $(document).ready(function() {
-    // your code here
+
+    $("#reset").click(function() {
+        $("#search-results").empty();
+    });
+    $("#search-btn").click(function() {
+        var userChoice = $("#search-query").val();
+        searchFlickr(clientID, userChoice, 10, function(photos) {
+            for (var i = 0; i < photos.photo.length; i++) {
+                photos.photo[i];
+                console.log(photos.photo[i]);
+                insertIntoPage(photos.photo[i]);
+                console.log();
+            }
+
+
+        });
+
+    });
 });
+
+function insertIntoPage(photoInfo) {
+    var url = '//www.flickr.com/photos/' + photoInfo.owner + '/' + photoInfo.id;
+    
+    $('#search-results').append("<a href=" + url + ">" + "<img src=" + photoInfo.url_m + ">  </img></a>");
+    
+}
+// + sign with .append
